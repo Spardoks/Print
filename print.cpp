45a319bc6cc91981ac38dcd85c5d97ef2c1db0f4
Spardoks <Vitasman98@yandex.ru>
2021-04-27 00:23:49 +0300

    base added

---
+++ print.cpp
@@ -0,0 +1,11 @@
+#include <print.hpp>
+#include <iostream>
+#include <string>
+
+namespace printtest {
+
+void print(const std::string& text) {
+  std::cout << text << std::endl;
+}
+
+} // namespace printtest
---
+++ print.hpp
@@ -0,0 +1,13 @@
+#ifndef PRINT_HPP
+#define PRINT_HPP
+
+#include <iostream>
+#include <string>
+
+namespace printtest {
+
+void print(const std::string& text);
+
+} // namespace printtest
+
+#endif // PRINT_HPP
